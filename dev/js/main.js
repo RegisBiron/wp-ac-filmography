f67f6a35bc1715612eafde123933d0ec6d6aa429
Regis Biron <rbiron13@gmail.com>
2014-11-23 17:01:50 -0500

    css and js authoring, nav, filter, top bar

--- dev/js/main.js
+++ dev/js/main.js
@@ -4,6 +4,9 @@ var direction;
 var lastScroll = 0;
 var winHeight = $(window).height();
 var docHeight = $(document).height();
+var countDown = 0;
+var countDownNum = ['5', '4', '3', '2', '1'];
+var timer;
 
 $(document).ready(function() {
 
@@ -22,9 +25,11 @@ $(document).ready(function() {
         if(navCounter >= 3){
             navCounter = 1;
             $('#bar-nav').removeClass('open');
+            $('.opacity-overlay').removeClass('active');
         }
         else{
             $('#bar-nav').addClass('open');
+            $('.opacity-overlay').addClass('active');
         }
     });
 
@@ -32,16 +37,41 @@ $(document).ready(function() {
         var currentScroll = window.pageYOffset;
         if(currentScroll > lastScroll && currentScroll >= 1){
             // console.log('down');
+            direction = 'down';
         }
         else if(currentScroll < lastScroll && currentScroll <= (docHeight - winHeight) - 1){
             // console.log('up');
+            direction = 'up';
         }
 
         lastScroll = currentScroll;
+        return direction;
     }
 
-    // $(document).scroll(function () {
-    //     getScrollDirection();
+    $(document).scroll(function () {
+        var currPos;
+        currPos = $(window).scrollTop();
+        getScrollDirection();
+        if(direction == 'up' && currPos >= 60){
+            $("#top-info-bar").addClass('in-view');
+        }
+        else if(direction == 'down' && currPos >= 60){
+            $("#top-info-bar").removeClass('in-view');
+        }
+    });
+
+    //film countdown animation
+    // $(function() {
+    //     filmCountDown();
+    //     function filmCountDown(){
+    //         console.log(countDownNum[countDown]);
+    //             timer = setTimeout(function() {
+    //             if(countDown <= countDownNum.length - 2){
+    //                 countDown++;
+    //                 filmCountDown();
+    //             }
+    //         }, 1000);
+    //     }
     // });
 
     // $(window).resize(function(){
